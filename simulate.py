66f8b35210fad7addd6362b7da39c79d6035be41
chloedhoff <cdhoff@uvm.edu>
2023-01-22 14:05:53 -0500

    created start of environmet

--- simulate.py
+++ simulate.py
@@ -1,2 +1,7 @@
 import pybullet as p
-pass
+
+#This creates an object, physicsClient, which handles the physics,
+#and draws the results to a Graphical User Interface (GUI).
+physicsClient = p.connect(p.GUI)
+p.disconnect()
+
