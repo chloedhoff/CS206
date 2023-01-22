3b67ef61aff3acc194014a3e510c86fcd4cab3b0
chloedhoff <cdhoff@uvm.edu>
2023-01-22 14:23:11 -0500

    opened environment

--- simulate.py
+++ simulate.py
@@ -1,7 +1,14 @@
 import pybullet as p
+import time
 
 #This creates an object, physicsClient, which handles the physics,
 #and draws the results to a Graphical User Interface (GUI).
 physicsClient = p.connect(p.GUI)
+
+for x in range(1000):
+   print(x)
+   p.stepSimulation()
+   time.sleep(1/60)
+
 p.disconnect()
 
