cd92b6437d87207ce158e449c49b5eca64d355dd
chloedhoff <cdhoff@uvm.edu>
2023-01-27 17:05:58 -0500

    finished step 29 in morelinks

--- simulate.py
+++ simulate.py
@@ -1,11 +1,16 @@
 import pybullet as p
 import time
+import pybullet_data
 
 #This creates an object, physicsClient, which handles the physics,
 #and draws the results to a Graphical User Interface (GUI).
-physicsClient = p.connect(p.GUI)
 
-p.loadSDF("box.sdf")
+physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+p.setGravity(0,0,-9.8)
+planeId = p.loadURDF("plane.urdf")
+p.loadSDF("boxes.sdf")
 
 for x in range(1000):
    print(x)
