5b962507e011df7a28cdf261582a775c9325078c
chloedhoff <cdhoff@uvm.edu>
2023-02-06 16:26:03 -0500

    added touch sensor to front leg

--- generate.py
+++ generate.py
@@ -8,20 +8,20 @@ def Create_World():
 def Create_Robot():
    pyrosim.Start_URDF("body.urdf")
    pyrosim.Send_Cube(name="BackLeg", pos=[x,y,z] , size=[length,width,height])
-   pyrosim.Send_Joint( name = "BackLeg_Torso" , parent= "BackLeg" , child = "Torso" , type = "revolute", position = [0, .5, 1])
-   pyrosim.Send_Cube(name="Torso", pos=[0,.5,.5] , size=[length,width,height])
-   pyrosim.Send_Joint( name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [0, 1, 0])  
-   pyrosim.Send_Cube(name="FrontLeg", pos=[0,.5,-.5] , size=[length,width,height])
+   pyrosim.Send_Joint( name = "BackLeg_Torso" , parent= "BackLeg" , child = "Torso" , type = "revolute", position = [1,.5,1])
+   pyrosim.Send_Cube(name="Torso", pos=[.5,0,.5] , size=[length,width,height])
+   pyrosim.Send_Joint( name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [1,0,0])  
+   pyrosim.Send_Cube(name="FrontLeg", pos=[.5,0,-.5] , size=[length,width,height])
    pyrosim.End()
 
 length = 1
 width = 1
 height = 1
-x = 0
-y = 0
+x = .5
+y = .5
 z = .5
 
-x2 = 0
+x2 = -5
 y2 = 3
  
 Create_World()
