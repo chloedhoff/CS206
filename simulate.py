57b34d3bf604db695381826381f091c420a3d556
chloedhoff <cdhoff@uvm.edu>
2023-02-06 16:25:35 -0500

    added touch sensor to front leg

--- simulate.py
+++ simulate.py
@@ -1,6 +1,7 @@
 import pybullet as p
 import time
 import pybullet_data
+import pyrosim.pyrosim as pyrosim
 
 #This creates an object, physicsClient, which handles the physics,
 #and draws the results to a Graphical User Interface (GUI).
@@ -10,12 +11,18 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
-p.loadSDF("boxes.sdf")
+robotId = p.loadURDF("body.urdf")
+p.loadSDF("world.sdf")
+
+pyrosim.Prepare_To_Simulate(robotId)
 
 for x in range(1000):
-   print(x)
+   #print(x)
    p.stepSimulation()
+   FrontLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg") 
+   print(FrontLegTouch)
    time.sleep(1/60)
+
 
 p.disconnect()
 
