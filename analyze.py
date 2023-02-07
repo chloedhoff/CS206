aaed9ecc546546287f8830e1fa485dd35f66daf6
chloedhoff <cdhoff@uvm.edu>
2023-02-06 21:19:58 -0500

    finished assignment

--- analyze.py
+++ analyze.py
@@ -1,5 +1,17 @@
 import numpy as np
+import matplotlib.pyplot
+
+
 
 FrontLegSensorValues = np.load("data/FrontLegSensorValues.npy")
 
+TorsoSensorValues = np.load("data/TorsoSensorValues.npy")
+
+matplotlib.pyplot.plot(FrontLegSensorValues, label = "Front Leg sensor", linewidth = 4)
+matplotlib.pyplot.plot(TorsoSensorValues, label = "Back Leg sensor")
+matplotlib.pyplot.legend(loc = "upper right")
+
 print(FrontLegSensorValues)
+print(TorsoSensorValues)
+
+matplotlib.pyplot.show()
